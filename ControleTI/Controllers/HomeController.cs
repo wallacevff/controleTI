168414d27846a3a4c0655a91ccba270f0087d33d
Wallace Vidal de Figueiredo Fortuna <wallacevff@hotmail.com>
2021-06-20 19:29:07 -0300

    Versão Finalizada!

--- ControleTI/Controllers/HomeController.cs
+++ ControleTI/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace ControleTI.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction("Index", "Dispositivos");
         }
 
         public IActionResult About()
