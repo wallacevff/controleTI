42092c3d397d04139a8f965fdac2b671226dd409
Wallace Vidal <wallacevff@hotmail.com>
2021-09-09 21:45:31 -0300

    Hardware Adicionado

--- ControleTI/Migrations/20210910003937_Hardware.Designer.cs
+++ ControleTI/Migrations/20210910003937_Hardware.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using ControleTI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ControleTI.Migrations
 {
     [DbContext(typeof(ControleTIContext))]
-    partial class ControleTIContextModelSnapshot : ModelSnapshot
+    [Migration("20210910003937_Hardware")]
+    partial class Hardware
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ControleTI/Migrations/20210910003937_Hardware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ControleTI.Migrations
+{
+    public partial class Hardware : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Hd",
+                table: "Dispositivo",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Memoria",
+                table: "Dispositivo",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Processador",
+                table: "Dispositivo",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hd",
+                table: "Dispositivo");
+
+            migrationBuilder.DropColumn(
+                name: "Memoria",
+                table: "Dispositivo");
+
+            migrationBuilder.DropColumn(
+                name: "Processador",
+                table: "Dispositivo");
+        }
+    }
+}
